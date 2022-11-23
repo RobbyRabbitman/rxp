5c1f3becb4f00e9c2c2cfa285bb22e06c8a0d005
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-11-23 21:16:29 +0100

    showcases: error handling

--- src/app/ui/ui-marble-graph/ui-marble.directive.ts
+++ src/app/ui/ui-marble-graph/ui-marble.directive.ts
@@ -1,19 +1,26 @@
 import { Directive, ElementRef, inject, Input } from '@angular/core';
-import { isMarbleWithValue, Marble } from 'src/app/model/marble';
+import {
+  isMarbleWithError,
+  isMarbleWithValue,
+  Marble,
+} from 'src/app/model/marble';
 
 @Directive({
   selector: '[appUiMarble]',
   standalone: true,
 })
 export class UiMarbleDirective {
-  private readonly element = inject(ElementRef<HTMLElement>);
+  private readonly element = inject<ElementRef<HTMLElement>>(ElementRef);
 
   @Input()
   public set appUiMarble(marble: Marble<unknown>) {
     const thumb = this.element.nativeElement.querySelector('.mat-slider-thumb');
-    if (thumb != null)
+    if (thumb != null) {
       thumb.innerHTML = String(
         isMarbleWithValue(marble) ? marble.value : marble.error
       );
+      if (isMarbleWithError(marble)) thumb.classList.add('marble-error');
+      else thumb.classList.remove('marble-error');
+    }
   }
 }
