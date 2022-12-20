723841805e5374ebeff8baca731932c0d90816ae
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-12-20 23:20:12 +0100

    github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, inject, NgModule } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
@@ -12,6 +14,7 @@ import { ShellComponent } from './components/smart/shell/shell.component';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     UiOperatorShowcaseComponent,
@@ -26,6 +29,20 @@ import { ShellComponent } from './components/smart/shell/shell.component';
           javascript: () => import('highlight.js/lib/languages/javascript'),
         },
       },
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        const registry = inject(MatIconRegistry);
+        const sanitizer = inject(DomSanitizer);
+
+        return () =>
+          registry.addSvgIcon(
+            'github',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg')
+          );
+      },
+      multi: true,
     },
   ],
   bootstrap: [AppComponent],
