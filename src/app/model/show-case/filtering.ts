9132fa776a8f7a2af63aeaf9f9b07eac406d6ce1
RobbyRabbitman <robby.rabbitman@gmail.com>
2023-01-04 00:24:56 +0100

    first

--- src/app/model/show-case/filtering.ts
+++ src/app/model/show-case/filtering.ts
@@ -1,4 +1,4 @@
-import { filter } from 'rxjs';
+import { filter, first } from 'rxjs';
 import { ShowCase } from './show-case';
 
 export const FILTER: ShowCase<[number], number> = {
@@ -19,6 +19,25 @@ export const FILTER: ShowCase<[number], number> = {
   operator: (graphs$) => graphs$[0].pipe(filter((x) => x % 2 === 0)),
 };
 
+export const FIRST: ShowCase<[number], number> = {
+  label: 'first',
+  operatorText: 'x$.pipe(first())',
+  graphs: [
+    {
+      end: 100,
+      marbles: [
+        { time: 10, value: 1 },
+        { time: 20, value: 2 },
+        { time: 55, value: 3 },
+        { time: 65, value: 4 },
+        { time: 75, value: 5 },
+      ],
+    },
+  ],
+  operator: (graphs$) => graphs$[0].pipe(first()),
+};
+
 export const SHOW_CASES_FILTERING = {
   filter: FILTER,
+  first: FIRST,
 };
