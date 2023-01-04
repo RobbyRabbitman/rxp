ef4d2975f717e5f315cae5f13b2b304d5b939076
RobbyRabbitman <robby.rabbitman@gmail.com>
2023-01-04 01:32:58 +0100

    throw error

--- src/app/model/show-case/creation.ts
+++ src/app/model/show-case/creation.ts
@@ -1,4 +1,4 @@
-import { interval } from 'rxjs';
+import { interval, observeOn, throwError } from 'rxjs';
 import { ShowCase } from './show-case';
 
 export const INTERVAL: ShowCase<[], number> = {
@@ -8,6 +8,15 @@ export const INTERVAL: ShowCase<[], number> = {
   operator: (_, scheduler) => interval(10, scheduler),
 };
 
+export const THROW_ERROR: ShowCase<[], number> = {
+  label: 'throw error',
+  operatorText: `throwError(() => new Error('whoopsi'))`,
+  graphs: [],
+  operator: (_, scheduler) =>
+    throwError(() => new Error('whoopsi')).pipe(observeOn(scheduler)),
+};
+
 export const SHOW_CASES_CREATION = {
   interval: INTERVAL,
+  throwError: THROW_ERROR,
 };
