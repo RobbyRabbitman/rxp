f263bddb88ba97877d85908cd4dadf11c6a266c7
RobbyRabbitman <robby.rabbitman@gmail.com>
2023-01-09 23:46:31 +0100

    rxjs link

---
+++ src/app/components/smart/rxjs-src-link/rxjs-src-link.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  InjectionToken,
+  Input,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+export const RXJS_SRC_LINK_BASE = new InjectionToken<string | undefined>('');
+
+@Component({
+  selector: 'app-rxjs-src-link',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  template: `<a mat-button color="primary" [href]="href" target="_blank"
+    ><mat-icon class="mr-4">folder_open</mat-icon>{{ href }}</a
+  >`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RxjsSrcLinkComponent {
+  public _repo = 'https://github.com/ReactiveX/rxjs';
+
+  @Input()
+  public base = inject(RXJS_SRC_LINK_BASE, { optional: true });
+
+  @Input()
+  public path?: string;
+
+  public get href() {
+    let href = this._repo;
+
+    if (this.base != null) href += `/${this.base}`;
+    if (this.path != null) href += `/${this.path}`;
+    return href;
+  }
+}
--- src/app/pages/show-case/show-case.component.ts
+++ src/app/pages/show-case/show-case.component.ts
@@ -4,21 +4,47 @@ import { MatDividerModule } from '@angular/material/divider';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { UiOperatorShowcaseComponent } from 'src/app/components/dumb/ui-operator-showcase/ui-operator-showcase.component';
+import {
+  RxjsSrcLinkComponent,
+  RXJS_SRC_LINK_BASE,
+} from 'src/app/components/smart/rxjs-src-link/rxjs-src-link.component';
 import { SHOW_CASES } from 'src/app/model/show-case/show-case';
 
 @Component({
   selector: 'app-show-case',
   standalone: true,
-  imports: [CommonModule, UiOperatorShowcaseComponent, MatDividerModule],
+  imports: [
+    CommonModule,
+    UiOperatorShowcaseComponent,
+    MatDividerModule,
+    RxjsSrcLinkComponent,
+  ],
   templateUrl: './show-case.component.html',
   styleUrls: ['./show-case.component.scss'],
+  providers: [
+    { provide: RXJS_SRC_LINK_BASE, useValue: 'blob/master/src/internal' },
+  ],
 })
 export class ShowCaseComponent {
+  public _categoryToPath: any = {
+    combination: 'operators',
+    conditional: 'operators',
+    creation: 'observable',
+    errorHandling: 'operators',
+    filtering: 'operators',
+    transformation: 'operators',
+    utility: 'operators',
+  };
+
   public showCases$ = inject(ActivatedRoute).params.pipe(
     map(({ category, id }) =>
       (SHOW_CASES as any)?.[category]?.[id] instanceof Array
         ? (SHOW_CASES as any)?.[category]?.[id]
         : [(SHOW_CASES as any)?.[category]?.[id]]
     )
+  );
+
+  public path$ = inject(ActivatedRoute).params.pipe(
+    map(({ category, id }) => `${this._categoryToPath[category]}/${id}.ts`)
   );
 }
